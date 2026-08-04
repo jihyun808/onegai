b30783c7b43e68fce39c3e241661ce4fcf6695bd
jihyun808 <jihyun8k@gmail.com>
2026-08-04 22:19:06 +0900

    Initial Commit

---
+++ karaokedayo/karaokedayoApp.swift
@@ -0,0 +1,17 @@
+//
+//  karaokedayoApp.swift
+//  karaokedayo
+//
+//  Created by 이지현 on 8/4/26.
+//
+
+import SwiftUI
+
+@main
+struct karaokedayoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
